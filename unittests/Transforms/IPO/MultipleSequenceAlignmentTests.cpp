5dff1a516265c18ed6d4aadbc2f61a6279b696a8
Yuta Saito <kateinoigakukun@gmail.com>
2022-06-08 10:59:27 +0000

    test: split TensorTableTests unittest

--- unittests/Transforms/IPO/MultipleSequenceAlignmentTests.cpp
+++ unittests/Transforms/IPO/MultipleSequenceAlignmentTests.cpp
@@ -1,6 +1,5 @@
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/ADT/TensorTable.h"
 #include "llvm/AsmParser/Parser.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Instruction.h"
@@ -16,32 +15,6 @@
 using namespace llvm;
 
 #define DEBUG_TYPE "msa-tests"
-
-TEST(TensorTableTest, DefaultValue) {
-  TensorTable<int32_t> t(std::vector<size_t>{2, 2}, 0);
-  for (size_t x = 0; x < 2; x++) {
-    for (size_t y = 0; y < 2; y++) {
-      auto v = t[{x, y}];
-      EXPECT_EQ(v, 0);
-    }
-  }
-}
-
-TEST(TensorTableTest, GetSet) {
-  TensorTable<int32_t> t(std::vector<size_t>{2, 2}, 0);
-  t.set({1, 0}, {0, 0}, false, 42);
-  auto v = t.get({1, 0}, {0, 0}, false);
-  EXPECT_EQ(v, 42);
-
-  v = t.get({0, 0}, {1, 0}, false);
-  EXPECT_EQ(v, 42);
-}
-
-TEST(TensorTableTest, Contains) {
-  TensorTable<int32_t> t(std::vector<size_t>{2, 2}, 0);
-  EXPECT_TRUE(t.contains({0, 0}, {0, 0}));
-  EXPECT_FALSE(t.contains({2, 0}, {0, 0}));
-}
 
 class MSAFunctionMergerAlignmentTest : public ::testing::Test {
 protected:
