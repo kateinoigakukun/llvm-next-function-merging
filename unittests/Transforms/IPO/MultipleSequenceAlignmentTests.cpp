a71b84d1475dd965b6d0f980720a7d09d6ff1cdb
Yuta Saito <kateinoigakukun@gmail.com>
2022-06-08 04:01:40 +0000

    test: parameterize alignment test

--- unittests/Transforms/IPO/MultipleSequenceAlignmentTests.cpp
+++ unittests/Transforms/IPO/MultipleSequenceAlignmentTests.cpp
@@ -1,9 +1,12 @@
 #include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/TensorTable.h"
 #include "llvm/AsmParser/Parser.h"
 #include "llvm/Transforms/IPO/MultipleSequenceAlignment.h"
 #include "llvm/Transforms/IPO/SALSSACodeGen.h"
 #include "gtest/gtest.h"
+#include <algorithm>
+#include <cstddef>
 #include <vector>
 
 using namespace llvm;
@@ -34,9 +37,29 @@ TEST(TensorTableTest, Contains) {
   EXPECT_FALSE(t.contains({2, 0}, {0, 0}));
 }
 
-TEST(MSAFunctionMergerTest, Alignment) {
+class MSAFunctionMergerAlignmentTest : public ::testing::Test {
+protected:
+  void withAlignment(Module &M, const std::vector<std::string> &FuncNames,
+                     function_ref<void(ArrayRef<MSAAlignmentEntry>)> Test) {
+    ASSERT_GT(M.getFunctionList().size(), 0);
+
+    FunctionMerger PairMerger(&M);
+    std::vector<Function *> Functions;
+    for (auto &FuncName : FuncNames) {
+      Functions.push_back(M.getFunction(FuncName));
+    }
+    MSAFunctionMerger Merger(Functions, PairMerger);
+    std::vector<MSAAlignmentEntry> Alignment;
+    Merger.align(Alignment);
+    std::reverse(Alignment.begin(), Alignment.end());
+    Test(Alignment);
+  }
+};
+
+TEST_F(MSAFunctionMergerAlignmentTest, Basic) {
   LLVMContext Ctx;
-  StringRef Text = R"(
+  SMDiagnostic Error;
+  auto M = parseAssemblyString(R"(
 declare void @extern_func_1()
 define internal void @Afunc(i32* %P, i32* %Q) {
   store i32 4, i32* %P
@@ -49,22 +72,18 @@ define internal void @Bfunc(i32* %P, i32* %Q) {
   store i32 6, i32* %Q
   ret void
 }
-  )";
+  )",
+                               Error, Ctx);
 
-  std::unique_ptr<Module> M;
-  {
-    SMDiagnostic Error;
-    M = parseAssemblyString(Text, Error, Ctx);
-  }
   ASSERT_TRUE(M);
-  ASSERT_GT(M->getFunctionList().size(), 0);
-
-  FunctionMerger PairMerger(M.get());
-  std::vector<Function *> Functions{M->getFunction("Afunc"),
-                                    M->getFunction("Bfunc")};
-  MSAFunctionMerger Merger(Functions, PairMerger);
-
-  std::vector<MSAAlignmentEntry> Alignment;
-  Merger.align(Alignment);
-  ASSERT_EQ(Alignment.size(), 4);
+  withAlignment(*M, {"Afunc", "Bfunc"}, [&](auto Alignment) {
+    ASSERT_EQ(Alignment.size(), 3);
+    for (auto &Entry : Alignment) {
+      Entry.dump();
+    }
+    ASSERT_TRUE(Alignment[0].match());
+    ASSERT_TRUE(Alignment[1].match());
+    ASSERT_FALSE(Alignment[2].match());
+    // ASSERT_TRUE(Alignment[3].match());
+  });
 }
