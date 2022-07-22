f2c0da5cad72fb488d3fbdba8f6b20f7f07cd547
Yuta Saito <kateinoigakukun@gmail.com>
2022-07-22 09:57:08 +0000

    test: add reduced miscompile test case

---
+++ test/Transforms/NextFM/CodeGen/mibench-automotive-susan/0_multiple-func-merging_Alignment.reduced.c
@@ -0,0 +1,28 @@
+// RUN: %clang -c -emit-llvm %s -o %t.bc
+// RUN: %opt --passes='multiple-func-merging' -func-merging-explore=1 -multiple-func-merging-allow-unprofitable %t.bc -o %t.opt.bc
+// RUN: %llc --filetype=obj %t.opt.bc -o %t.opt.o
+// RUN: %clang -Wno-all -Wno-pointer-sign -Wno-literal-conversion %t.opt.o %S/Inputs/0_multiple-func-merging_Alignment.reduced.driver.c -lm -o %t.opt
+// RUN: %t.opt %S/Inputs/input_small.pgm %t/output_small.edges.pgm -e
+// XFAIL: *
+
+typedef unsigned char uchar;
+
+int susan_edges(uchar *in, int *r, uchar *mid, uchar *bp, int max_no,
+                int x_size, int y_size) {
+
+  for (int i = 0; i < y_size; i++) {
+    x_size += *in;
+  }
+  return 0;
+}
+
+int susan_edges_small(uchar *in, int *r, uchar *mid, uchar *bp, int max_no,
+                      int x_size, int y_size) {
+  int n;
+  uchar *p;
+
+  for (int i = 0; i < y_size; i++) {
+    n += *p;
+  }
+  return 0;
+}
