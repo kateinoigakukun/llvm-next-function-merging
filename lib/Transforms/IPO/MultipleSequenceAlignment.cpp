fa1085abeafdf73aba4c26a61f18836425215d5f
Yuta Saito <kateinoigakukun@gmail.com>
2022-07-23 11:44:44 +0000

    code: rename predefinedArgs -> preservedArgs

--- lib/Transforms/IPO/MultipleSequenceAlignment.cpp
+++ lib/Transforms/IPO/MultipleSequenceAlignment.cpp
@@ -1672,7 +1672,7 @@ void MSAGenFunction::layoutParameters(
   assert(Functions.size() > 0 && "No functions to merge!");
   Args.emplace_back(DiscriminatorTy, AttributeSet());
 
-  size_t predefinedArgs = Args.size();
+  size_t preservedArgs = Args.size();
   auto FindReusableArg =
       [&](Argument *NewArg, AttributeSet NewAttr,
           const std::set<unsigned> &reusedArgs) -> Optional<int> {
@@ -1690,7 +1690,7 @@ void MSAGenFunction::layoutParameters(
     // In the above example, %d can be reused with both %c and %a,
     // but %a is better to avoid additonal select.
 
-    for (size_t i = predefinedArgs; i < Args.size(); i++) {
+    for (size_t i = preservedArgs; i < Args.size(); i++) {
       Type *ty;
       AttributeSet attr;
       std::tie(ty, attr) = Args[i];
