6ce07a47ec4c2d47232e8b065b8644ec93fdaca0
Yuta Saito <kateinoigakukun@gmail.com>
2022-08-06 12:31:33 +0000

    code: encode optional bit by bitmask to reduce size

--- unittests/Transforms/IPO/FunctionMergingUtilsTests.cpp
+++ unittests/Transforms/IPO/FunctionMergingUtilsTests.cpp
@@ -5,9 +5,11 @@ using namespace llvm::fmutils;
 
 TEST(OptionalScoreTest, Basic) {
   OptionalScore S1(1);
+  ASSERT_TRUE(S1.hasValue());
   ASSERT_EQ(*S1, 1);
 
   OptionalScore S2 = OptionalScore::min();
+  ASSERT_TRUE(S2.hasValue());
   ASSERT_EQ(*S2, OptionalScore::min());
 
   OptionalScore S3 = OptionalScore::None();
