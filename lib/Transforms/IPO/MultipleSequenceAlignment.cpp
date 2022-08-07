1b3bd866993b00cadc8ee1d4aa386b1653a4b5f7
Yuta Saito <kateinoigakukun@gmail.com>
2022-08-07 07:16:55 +0000

    debug: emit remarks even if only-function

--- lib/Transforms/IPO/MultipleSequenceAlignment.cpp
+++ lib/Transforms/IPO/MultipleSequenceAlignment.cpp
@@ -2023,6 +2023,8 @@ PreservedAnalyses MultipleFunctionMergingPass::run(Module &M,
     MSAStats Stats;
     auto maybePlan = FM.planMerge(Stats, Options);
     if (auto plan = maybePlan) {
+      auto score = plan->computeScore(FAM);
+      score.emitPassedRemark(*plan, ORE);
       auto &Merged = plan->applyMerge(FAM, ORE);
     }
     return PreservedAnalyses::none();
