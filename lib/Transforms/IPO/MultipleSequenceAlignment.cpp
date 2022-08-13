kateinoigakukun/llvm-next-function-merging

commit 1a5ea2cc51e10e6b9d002a705d03f51365f78199

fix: optimize switch -> cond_br for bb selection when 2 path

lib/Transforms/IPO/MultipleSequenceAlignment.cpp
@@ -1074,6 +1074,18 @@ bool MSAGenFunctionBody::assignMergedInstLabelOperands(
 
     if (areAllOperandsEqual) {
       V = Vs[0]; // assume that V1 == V2 == ... == Vn
+    } else if (Vs.size() == 2) {
+      assert(Instructions.size() == 2 && "Invalid number of instructions!");
+      // if there are only two instructions, we can just use cond_br
+      auto *SelectBB = BasicBlock::Create(Parent.C, "bb.select.bb", MergedFunc);
+      IRBuilder<> Builder(SelectBB);
+      Builder.CreateCondBr(Discriminator, dyn_cast<BasicBlock>(Vs[0]),
+                           dyn_cast<BasicBlock>(Vs[1]));
+      for (size_t FuncId = 0, e = Instructions.size(); FuncId < e; ++FuncId) {
+        MergedBBToBB[FuncId][SelectBB] = Instructions[FuncId]->getParent();
+      }
+      MergedBBToBB[0][SelectBB] = I->getParent();
+      V = SelectBB;
     } else {
       auto *SelectBB = BasicBlock::Create(Parent.C, "bb.select.bb", MergedFunc);
       IRBuilder<> BuilderBB(SelectBB);

test/Transforms/NextFM/CodeGen/libjpeg/read_rle_pixel_read_non_rle_pixel.ll
@@ -7,7 +7,6 @@
 ; RUN: %strip %t.mfm.o
 ; RUN: %strip %t.fm.o
 ; RUN: [[ $(stat -c%%s %t.mfm.o) -le $(stat -c%%s %t.fm.o) ]]
-; XFAIL: *
 
 ; ModuleID = '../benchmarks/mibench/consumer/jpeg/cjpeg.bc'
 source_filename = "llvm-link"

commit c4b4e4131ab2579249ee31f5b7386b4c6f98ca5e

fix: exchange cond_br's true_bb and false_bb
The wrong order was introduced by 1a5ea2cc51e10e6b9d002a705d03f51365f78199

lib/Transforms/IPO/MultipleSequenceAlignment.cpp
@@ -1080,8 +1080,8 @@ bool MSAGenFunctionBody::assignMergedInstLabelOperands(
       // if there are only two instructions, we can just use cond_br
       auto *SelectBB = BasicBlock::Create(Parent.C, "bb.select.bb", MergedFunc);
       IRBuilder<> Builder(SelectBB);
-      Builder.CreateCondBr(Discriminator, dyn_cast<BasicBlock>(Vs[0]),
-                           dyn_cast<BasicBlock>(Vs[1]));
+      Builder.CreateCondBr(Discriminator, dyn_cast<BasicBlock>(Vs[1]),
+                           dyn_cast<BasicBlock>(Vs[0]));
       for (size_t FuncId = 0, e = Instructions.size(); FuncId < e; ++FuncId) {
         MergedBBToBB[FuncId][SelectBB] = Instructions[FuncId]->getParent();
       }