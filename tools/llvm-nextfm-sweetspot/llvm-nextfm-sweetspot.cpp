d14e8dd8fe0211953e2ff0c11de4ed6f1708997f
Yuta Saito <kateinoigakukun@gmail.com>
2022-09-19 00:23:06 +0000

    tools: add best merge finder tool

--- tools/llvm-nextfm-sweetspot/llvm-nextfm-sweetspot.cpp
+++ tools/llvm-nextfm-sweetspot/llvm-nextfm-sweetspot.cpp
@@ -1,3 +1,4 @@
+#include "llvm/ADT/None.h"
 #include "llvm/ADT/SetVector.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/Twine.h"
@@ -10,18 +11,25 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/IRReader/IRReader.h"
+#include "llvm/Passes/PassBuilder.h"
 #include "llvm/Remarks/Remark.h"
 #include "llvm/Remarks/RemarkParser.h"
 #include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/Error.h"
 #include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Host.h"
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/JSON.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetOptions.h"
 #include "llvm/Transforms/IPO.h"
 #include "llvm/Transforms/Utils/Cloning.h"
 #include <algorithm>
@@ -53,13 +61,18 @@ static cl::opt<OutputFileType>
                         ),
              cl::cat(SweetSpotCat));
 
+static cl::list<std::string>
+    DeletingFunctions("del-func",
+                      cl::desc("Delete the specified function from the module"),
+                      cl::cat(SweetSpotCat));
+
 static cl::opt<std::string> OutputFile("o", cl::desc("<output dir>"),
-                                       cl::Required, cl::cat(SweetSpotCat));
+                                       cl::Optional, cl::cat(SweetSpotCat));
 static cl::opt<std::string> StatsFile("delete-stats",
                                       cl::desc("<stats output file>"),
                                       cl::Optional, cl::cat(SweetSpotCat));
 static cl::opt<std::string> RemarkFile("remark", cl::desc("<remark file>"),
-                                       cl::Required, cl::cat(SweetSpotCat));
+                                       cl::Optional, cl::cat(SweetSpotCat));
 static cl::opt<std::string> ParserFormat("format",
                                          cl::desc("The format of the remarks."),
                                          cl::init("yaml"),
@@ -215,32 +228,76 @@ EstimateBestReduction(MemoryBuffer &RemarkFileBuf, Module *M, Stats &S) {
   return std::move(NewM);
 }
 
-int main(int argc, char **argv) {
-  InitLLVM X(argc, argv);
+struct MergeEstimation {
+  size_t ReductionBytes;
+  SetVector<StringRef> DeletedFunctions;
+};
 
-  LLVMContext Context;
-  cl::HideUnrelatedOptions(SweetSpotCat);
-  cl::ParseCommandLineOptions(argc, argv, "llvm NextFM test generator\n");
-
-  SMDiagnostic Err;
-  std::unique_ptr<Module> M = parseIRFile(InputFile, Err, Context);
-
-  if (!M.get()) {
-    Err.print(argv[0], errs());
-    return 1;
+static bool DeletedObjectFunctions(SetVector<StringRef> &DeletedFunctions,
+                                   Module *M, raw_pwrite_stream &OS) {
+  for (auto FuncName : DeletedFunctions) {
+    GlobalValue *GV = M->getFunction(FuncName);
+    if (!GV) {
+      errs() << "Function " << FuncName << " not found in module\n";
+      return true;
+    }
+  }
+  std::string Err;
+  auto *Target = TargetRegistry::lookupTarget(M->getTargetTriple(), Err);
+  if (Err.size()) {
+    errs() << "Error: " << Err << "\n";
   }
 
-  ErrorOr<std::unique_ptr<MemoryBuffer>> Buf =
-      MemoryBuffer::getFile(RemarkFile);
-  if (std::error_code EC = Buf.getError()) {
-    WithColor::error() << "Can't open file " << RemarkFile << ": "
-                       << EC.message() << "\n";
-    return 1;
+  TargetOptions opt;
+  auto TM = Target->createTargetMachine(M->getTargetTriple(), "generic", "",
+                                        opt, None);
+
+  auto NewM = DeleteFunctions(DeletedFunctions, M);
+
+  legacy::PassManager pass;
+  auto FileType = CGFT_ObjectFile;
+
+  if (TM->addPassesToEmitFile(pass, OS, nullptr, FileType)) {
+    errs() << "TheTargetMachine can't emit a file of this type";
+    return true;
   }
 
+  pass.run(*NewM);
+  return false;
+}
+
+static bool
+FindBestMissingCandidates(MemoryBuffer &RemarkFileBuf, Module *M,
+                          std::vector<MergeEstimation> &Estimations) {
+  auto MissedRemarks = CollectMissedRemarks(RemarkFileBuf);
+
+  for (auto &Remark : MissedRemarks) {
+    bool hasSkippedFirst = false;
+    SetVector<StringRef> DeletedFunctions;
+    for (const remarks::Argument &Arg : Remark->Args) {
+      if (Arg.Key == "Function") {
+        if (hasSkippedFirst) {
+          DeletedFunctions.insert(Arg.Val);
+        } else {
+          hasSkippedFirst = true;
+        }
+      }
+    }
+    SmallString<0> CodeString;
+    raw_svector_ostream OStream(CodeString);
+    if (DeletedObjectFunctions(DeletedFunctions, M, OStream))
+      continue;
+
+    dbgs() << "Reduction: " << CodeString.size() << " bytes\n";
+    Estimations.push_back({CodeString.size(), DeletedFunctions});
+  }
+  return false;
+}
+
+int bestReductionMain(MemoryBuffer &Buf, Module *M) {
   // Handle remark files.
   Stats stats;
-  auto NewM = EstimateBestReduction(**Buf, M.get(), stats);
+  auto NewM = EstimateBestReduction(Buf, M, stats);
 
   if (!NewM) {
     return 1;
@@ -274,6 +331,79 @@ int main(int argc, char **argv) {
     break;
   }
   PM.run(*NewM);
+  return 0;
+}
+
+int bestMergeMain(MemoryBuffer &Buf, Module *M) {
+  std::vector<MergeEstimation> Estimations;
+  auto hasError = FindBestMissingCandidates(Buf, M, Estimations);
+
+  if (hasError) {
+    return 1;
+  }
+
+  for (auto &Estimation : Estimations) {
+    json::Array DeletedFunctions;
+    for (auto &DeletedFunction : Estimation.DeletedFunctions) {
+      DeletedFunctions.push_back(DeletedFunction);
+    }
+    json::Object obj{
+        {"ReductionBytes", (uint32_t)Estimation.ReductionBytes},
+        {"DeletedFunctions", json::Value(std::move(DeletedFunctions))},
+    };
+    std::error_code EC;
+    raw_fd_ostream OS(StatsFile, EC, sys::fs::OF_None);
+    OS << json::Value(std::move(obj));
+  }
 
   return 0;
+}
+
+int main(int argc, char **argv) {
+  InitLLVM X(argc, argv);
+
+  LLVMContext Context;
+  cl::HideUnrelatedOptions(SweetSpotCat);
+  cl::ParseCommandLineOptions(argc, argv, "llvm NextFM test generator\n");
+
+  SMDiagnostic Err;
+  std::unique_ptr<Module> M = parseIRFile(InputFile, Err, Context);
+
+  if (!M.get()) {
+    Err.print(argv[0], errs());
+    return 1;
+  }
+
+  if (true) {
+    InitializeNativeTarget();
+    InitializeNativeTargetAsmParser();
+    InitializeNativeTargetAsmPrinter();
+    SetVector<StringRef> DeletedFunctions;
+    for (auto &FuncName : DeletingFunctions) {
+      DeletedFunctions.insert(FuncName);
+    }
+    std::error_code EC;
+    raw_fd_ostream OS(OutputFile, EC, sys::fs::OF_None);
+    if (EC) {
+      errs() << "Error creating temporary file: \n";
+      errs() << EC.message() << '\n';
+      return 1;
+    }
+
+    if (DeletedObjectFunctions(DeletedFunctions, M.get(), OS)) {
+      errs() << "Error while deleting functions";
+      return 1;
+    }
+    return 0;
+  } else {
+    ErrorOr<std::unique_ptr<MemoryBuffer>> Buf =
+        MemoryBuffer::getFile(RemarkFile);
+    if (std::error_code EC = Buf.getError()) {
+      WithColor::error() << "Can't open file " << RemarkFile << ": "
+                         << EC.message() << "\n";
+      return 1;
+    }
+
+    return bestReductionMain(*Buf.get(), M.get());
+  }
 }
--- tools/llvm-nextfm-sweetspot/sweetspot.rb
+++ tools/llvm-nextfm-sweetspot/sweetspot.rb
@@ -1,8 +1,10 @@
 require "fileutils"
 require "json"
+require "open3"
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
 require "markdown"
+require_relative "../llvm-nextfm-remark/llvm-nextfm-remark"
 
 def process_remark(remark, tmpdir, bmark_out, tool, llc)
   name = File.basename(File.dirname(remark))
@@ -43,25 +45,84 @@ rescue => e
   nil
 end
 
-if __FILE__ == $0
-  bmark_out = "/home/katei/ghq/github.com/kateinoigakukun/llvm-next-function-merging/build/bench-play/analysis/rustc-perf/plot-data/3-function-merge"
-  tool = "/home/katei/ghq/github.com/kateinoigakukun/llvm-next-function-merging/build/release-13-rustc/tools/llvm-nextfm-sweetspot/llvm-nextfm-sweetspot"
-  llc = "llc-13"
-  tmpdir = "/tmp/llvm-nextfm-sweetspot"
-  table = Markdown::Table.new([
-    ["Benchmark", :left],
-    ["Deleted Functions", :right], ["Deleted Functions (%)", :right],
-    ["Reduction (bytes)", :right], ["Reduction (%)", :right],
-  ])
-  workers = Dir.glob("#{bmark_out}/*/remarks.yaml").map do |remark|
-    Thread.new do
-      process_remark(remark, tmpdir, bmark_out, tool, llc)
-    end
+def find_best_merge_candidate(remark, tool, obj_bc, db)
+  # skip if we already have a candidate
+  func_names = remark.funcs.join(",")
+  if db.execute("SELECT * FROM missed_merges WHERE func_names = ?", func_names).size > 0
+    puts "\x1b[1;33m[SKIP]\x1b[m #{func_names}"
+    return
+  end
+  require "tempfile"
+  Tempfile.create("sweetspot") do |f|
+    args = [tool, obj_bc, "-o", f.path]
+    args += remark.funcs.map { |f| "--del-func=#{f}" }
+    puts "\x1b[1;36m[RUN ]\x1b[m #{args.map{ "'#{_1}'" }.join(" ")}"
+    Kernel.system(*args, exception: true)
+    strip_args = ["strip", "-s", f.path]
+    Kernel.system(*strip_args, exception: true)
+    size = File.size(f.path)
+    db.execute("INSERT INTO missed_merges (func_names, bytes) VALUES (?, ?)", [func_names, size])
+    puts "\x1b[1;32m[OK  ]\x1b[m #{size}: #{func_names}"
   end
 
-  workers.each do |worker|
-    row = worker.value
-    table.add_row(row) if row
-    puts table.to_markdown
+rescue => e
+  puts ""
+  puts "\x1b[1;31m[FAIL]\x1b[m #{func_names}"
+  nil
+end
+
+def create_db_skeleton(db)
+  db.execute("CREATE TABLE IF NOT EXISTS missed_merges (func_names TEXT, bytes INTEGER)")
+end
+
+FIND_PAIR = true
+
+if __FILE__ == $0
+  tool = "/home/katei/ghq/github.com/kateinoigakukun/llvm-next-function-merging/build/release-13-rustc-llvmrel/tools/llvm-nextfm-sweetspot/llvm-nextfm-sweetspot"
+  if FIND_PAIR
+    require 'sqlite3'
+    remark_file = ARGV[0]
+    name = File.basename(File.dirname(remark_file))
+    obj_bc = File.join(File.dirname(remark_file), "obj.bc")
+    db = SQLite3::Database.new "test.db"
+    create_db_skeleton(db)
+    works = RemarkSet.load_file(remark_file).remarks.to_a.reverse.filter_map do |remark|
+      next unless remark.is_a?(MergeMissedRemark)
+      proc {
+        find_best_merge_candidate(remark, tool, obj_bc, db)
+      }
+    end
+    worker_count = 8
+    batch_size = works.size / worker_count
+    workers = works.each_slice(batch_size).to_a.map do |works|
+      Thread.new {
+        works.each do |work|
+          work.call
+        end
+      }
+    end
+    workers.each do |worker|
+      row = worker.join
+    end
+  else
+    bmark_out = "/home/katei/ghq/github.com/kateinoigakukun/llvm-next-function-merging/build/bench-play/analysis/rustc-perf/plot-data/3-function-merge"
+    tmpdir = "/tmp/llvm-nextfm-sweetspot"
+    llc = "llc-13"
+    table = Markdown::Table.new([
+      ["Benchmark", :left],
+      ["Deleted Functions", :right], ["Deleted Functions (%)", :right],
+      ["Reduction (bytes)", :right], ["Reduction (%)", :right],
+    ])
+    workers = Dir.glob("#{bmark_out}/*/remarks.yaml").map do |remark|
+      Thread.new do
+        process_remark(remark, tmpdir, bmark_out, tool, llc)
+      end
+    end
+
+    workers.each do |worker|
+      row = worker.value
+      table.add_row(row) if row
+      puts table.to_markdown
+    end
   end
 end
