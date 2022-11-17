0456b3fc2a3c3709f3b61c14124cfc7e052eede6
Yuta Saito <kateinoigakukun@gmail.com>
2022-11-17 08:17:17 +0000

    Revert "fix: handling for identical type pointers"
    This reverts commit 85626e885133fffcf2f4cf08e9e0f6d53f1240de.
    
    because it broke
    test/Transforms/NextFM/CodeGen/rustc-perf/_ZN8regalloc7bt_main10alloc_main.ll
    and others

--- lib/Transforms/IPO/FunctionMerging.cpp
+++ lib/Transforms/IPO/FunctionMerging.cpp
@@ -314,7 +314,7 @@ static bool CmpTypes(Type *TyL, Type *TyR, const DataLayout *DL) {
     TyR = DL->getIntPtrType(TyR);
 
   if (TyL == TyR)
-    return true;
+    return false;
 
   switch (TyL->getTypeID()) {
   default:
