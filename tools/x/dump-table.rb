f5b56f64d4abbe406e80ec5c3cdfbb473d81fb86
Yuta Saito <kateinoigakukun@gmail.com>
2023-01-21 23:25:20 +0000

    tools: normalize variant name and format tables

--- tools/x/dump-table.rb
+++ tools/x/dump-table.rb
@@ -35,9 +35,11 @@ module TexReport
 
   def self.generate_objsize_csv(result)
     results = self.stats_objsize(result)
-    variants = results.values.map(&:keys).flatten.uniq.sort
-    csv = "testsuite," + variants.join(",")
+    all_variants = results.values.map(&:keys).flatten.uniq.sort
+    csv = "testsuite," + all_variants.map { |v| self.normalize_variant_name(v) }.join(",")
     results.each do |testsuite, variants|
+      next if (variants.keys & all_variants).size != all_variants.size
+      next if variants.values.uniq.size == 1
       csv += "\n"
       csv += "#{testsuite}," + variants.values.map(&:to_s).join(",")
     end
@@ -46,16 +48,32 @@ module TexReport
 
   def self.generate_table(result)
     results = self.stats_objsize(result)
-    variants = results.values.map(&:keys).flatten.uniq.sort
+    all_variants = results.values.map(&:keys).flatten.uniq.sort
     puts "\\begin{table}[ht]"
     puts "\\centering"
-    puts "\\begin{tabular}{l#{'r' * variants.size}}"
+    puts "\\begin{tabular}{l#{'r' * all_variants.size}}"
     puts "\\hline"
-    puts "Test Suite & #{variants.join(' & ')} \\\\"
+    puts "Test Suite & #{all_variants.map do |v|
+      self.normalize_variant_name(v)
+    end.join(' & ')} \\\\"
     puts "\\hline"
     puts "\\hline"
     results.each do |testsuite, variants|
-      puts "#{self.escape testsuite} & #{variants.values.map { |v| v.join(' & ') }.join(' & ')} \\\\"
+      next if (variants.keys & all_variants).size != all_variants.size
+      baseline = variants["baseline"]
+      row = "#{self.escape testsuite} &"
+      row += all_variants.map do |variant|
+        next "N/A" unless variants[variant]
+        next variants[variant].to_s if variant == "baseline"
+        size = variants[variant]
+        diff = size - baseline
+        diff = diff.abs
+        percent = diff.to_f / baseline * 100
+        # SIZE (DIFF%)
+        "#{size} (#{percent.round(1)}\\%)"
+      end.join(' & ')
+      row += " \\\\"
+      puts row
     end
     puts "\\hline"
     puts "\\end{tabular}"
@@ -69,6 +87,16 @@ module TexReport
   def self.normalize_testsuite_name(name)
     name.sub(/_test$/, "")
   end
+
+  def self.normalize_variant_name(name)
+    return "F3M" if name == "fm"
+    case name
+    when "fm" then "F3M"
+    when "baseline" then "Baseline"
+    when /mfm\+(\d+)/ then "MFM [e=#{$1}]"
+    else raise "Unknown variant: #{name}"
+    end
+  end
 end
 
 working_dir = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", ".x"))
@@ -78,6 +106,8 @@ Dir.chdir working_dir do
   ci = ContinuousIntegration.new
   run_id = ci.latest_successful_run_id
   result = ci.download_ci_artifacts(run_id)
+  method = ARGV[0]
+  TexReport.send(method, result)
   # TexReport.generate_table(result)
-  TexReport.generate_objsize_csv(result)
+  # TexReport.generate_objsize_csv(result)
 end
