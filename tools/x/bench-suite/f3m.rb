70fd028557f46ed2e31ea3ddda72adc376e1a584
Yuta Saito <kateinoigakukun@gmail.com>
2023-02-12 05:52:44 +0000

    tools: add f3m benchmark harness

--- tools/x/bench-suite/f3m.rb
+++ tools/x/bench-suite/f3m.rb
@@ -4,6 +4,7 @@ require_relative "../build"
 class F3M < Rake::TaskLib
   def initialize(build)
     @build = build
+    @suite_dir = File.expand_path(File.dirname(__FILE__))
   end
 
   def download
@@ -20,12 +21,20 @@ class F3M < Rake::TaskLib
     unless File.exist? path
       mkdir_p path
       sh "tar xf #{tar} -C #{path} --strip-components=1"
+      sh "patch -p0 < #{File.join(@suite_dir, "f3m", "setup.sh.patch")}", chdir: path
+      # Build patched GNU time
+      sh "bash ./setup.sh", chdir: path
     end
+    path
   end
 
   def run
     tar = download
-    extract tar
+    dir = extract tar
+
+    fakebin = File.join(@build.dot_x_dir, "bench-suite", "f3m", "llvm-fakebin")
+    sh ({ LLVM_NEXTFM_PLUGIN: @build.pass_plugin }),
+      "./run_exps.py --llvm_dir #{fakebin} spec2006", chdir: dir
   end
 end
 
