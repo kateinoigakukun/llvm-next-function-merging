e6323b341b2cf2670d38c8042161efcd07294ccd
Yuta Saito <kateinoigakukun@gmail.com>
2023-02-19 10:36:58 +0000

    tools: filter non-changed benchmarks out

--- tools/x/bench-suite/f3m/plot.py
+++ tools/x/bench-suite/f3m/plot.py
@@ -49,7 +49,28 @@ class Missed(yaml.YAMLObject):
 
 class DataSource:
     def y_labels(self):
-        return sorted(self.data.keys())
+        # return sorted(self.data.keys())
+        return [
+            '400.perlbench',
+            '401.bzip2',
+            # '403.gcc',
+            # '429.mcf',
+            # '433.milc',
+            '444.namd',
+            '445.gobmk',
+            # '447.dealII',
+            # '450.soplex',
+            # '453.povray',
+            '456.hmmer',
+            # '458.sjeng',
+            '462.libquantum',
+            # '464.h264ref',
+            # '470.lbm',
+            '471.omnetpp',
+            # '473.astar',
+            # '482.sphinx3',
+            '483.xalancbmk'
+        ]
 
     def baseline_case_name(self):
         return "TECHNIQUE=baseline"
