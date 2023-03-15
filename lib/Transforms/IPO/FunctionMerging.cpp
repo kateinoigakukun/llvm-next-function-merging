2223eedcb952a4ffb4a3fc3a49aae90bf005f23e
Yuta Saito <kateinoigakukun@gmail.com>
2023-03-15 06:01:49 +0000

    code: extract Fingerprint

--- lib/Transforms/IPO/FunctionMerging.cpp
+++ lib/Transforms/IPO/FunctionMerging.cpp
@@ -112,6 +112,7 @@
 #include "llvm/Analysis/InlineSizeEstimatorAnalysis.h"
 
 #include "llvm/ADT/SANeedlemanWunsch.h"
+#include "llvm/Transforms/IPO/Fingerprint.h"
 #include "llvm/Transforms/IPO/SALSSACodeGen.h"
 
 #include <algorithm>
@@ -1543,12 +1544,6 @@ void FunctionMerger::CodeGenerator<BlockListType>::destroyGeneratedCode() {
 
 unsigned instToInt(Instruction *I);
 
-inst_range getInstructions(Function *F) { return instructions(F); }
-
-iterator_range<BasicBlock::iterator> getInstructions(BasicBlock *BB) {
-  return make_range(BB->begin(), BB->end());
-}
-
 template <class T> class FingerprintMH {
 private:
   // The number of instructions defining a shingle. 2 or 3 is best.
@@ -1658,57 +1653,6 @@ public:
     size_t nintersect = nHashes - (mismatches / 2);
     int nunion = 2 * nHashes - nintersect;
     return 1.f - (nintersect / (float)nunion);
-  }
-};
-
-template <class T> class Fingerprint {
-public:
-  uint64_t magnitude{0};
-  static const size_t MaxOpcode = 68;
-  std::array<uint32_t, MaxOpcode> OpcodeFreq;
-
-  Fingerprint() = default;
-
-  Fingerprint(T owner) {
-    // memset(OpcodeFreq, 0, sizeof(int) * MaxOpcode);
-    for (size_t i = 0; i < MaxOpcode; i++)
-      OpcodeFreq[i] = 0;
-
-    for (Instruction &I : getInstructions(owner)) {
-      OpcodeFreq[I.getOpcode()]++;
-      if (I.isTerminator())
-        OpcodeFreq[0] += I.getNumSuccessors();
-    }
-    for (size_t i = 0; i < MaxOpcode; i++) {
-      uint64_t val = OpcodeFreq[i];
-      magnitude += val * val;
-    }
-  }
-
-  uint32_t footprint() const { return sizeof(int) * MaxOpcode; }
-
-  float distance(const Fingerprint &FP2) const {
-    int Distance = 0;
-    for (size_t i = 0; i < MaxOpcode; i++) {
-      int Freq1 = OpcodeFreq[i];
-      int Freq2 = FP2.OpcodeFreq[i];
-      Distance += std::abs(Freq1 - Freq2);
-    }
-    return static_cast<float>(Distance);
-  }
-};
-
-class BlockFingerprint : public Fingerprint<BasicBlock *> {
-public:
-  BasicBlock *BB{nullptr};
-  size_t Size{0};
-
-  BlockFingerprint(BasicBlock *BB) : Fingerprint(BB), BB(BB) {
-    for (Instruction &I : *BB) {
-      if (!isa<LandingPadInst>(&I) && !isa<PHINode>(&I)) {
-        Size++;
-      }
-    }
   }
 };
 
