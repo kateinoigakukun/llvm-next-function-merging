ee5e28b2db140564b868af69f032b86b683ea0d7
Yuta Saito <kateinoigakukun@gmail.com>
2024-06-30 08:20:53 +0000

    code: try to merge only twice

--- lib/Transforms/IPO/MultipleSequenceAlignment.cpp
+++ lib/Transforms/IPO/MultipleSequenceAlignment.cpp
@@ -2231,17 +2231,20 @@ public:
 
   void exploreProfitableSet(SmallVectorImpl<Function *> &Functions,
                             bool IdenticalTypesOnly) {
+    if (!IdenticalType) {
+      tryPlanMerge(Functions, false);
+    }
+    tryPlanMerge(Functions, true);
+    // auto tryPlanAllSets = [&](const PartitionSetTy &PartitionIndicesSet) {
+    //   if (!IdenticalType) {
+    //     tryPlanMerge(Functions, PartitionIndicesSet, false);
+    //   }
+    //   tryPlanMerge(Functions, PartitionIndicesSet, true);
+    // };
 
-    auto tryPlanAllSets = [&](const PartitionSetTy &PartitionIndicesSet) {
-      if (!IdenticalType) {
-        tryPlanMerge(Functions, PartitionIndicesSet, false);
-      }
-      tryPlanMerge(Functions, PartitionIndicesSet, true);
-    };
-
-    fmutils::SetPartitions S(Functions.size(),
-                             [&](const auto &Set) { tryPlanAllSets(Set); });
-    S.iterateOverPartitions();
+    // fmutils::SetPartitions S(Functions.size(),
+    //                          [&](const auto &Set) { tryPlanAllSets(Set); });
+    // S.iterateOverPartitions();
   }
 
   Optional<PlanResult> getBestPlan() { return bestPlan; }
